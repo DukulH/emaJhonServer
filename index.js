af6411a4e5f3c1ed5b6fea72bc91c1c93853fcb9
DukulH <dukul.howlader30@gmail.com>
2021-04-06 22:55:16 +0600

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config()
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7mlny.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
