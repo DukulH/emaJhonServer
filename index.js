e7d42d5d54501716e6478b0c71c1770c89ab4e0a
DukulH <dukul.howlader30@gmail.com>
2021-04-06 21:41:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7mlny.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const port = 5000;
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productsCollection = client.db("emaJohnShop").collection("Products");
+    const ordersCollection = client.db("emaJohnShop").collection("Orders");
+
+    app.post('/addProduct', (req, res) => {
+        const products = req.body;
+        productsCollection.insertOne(products)
+        .then(result => {
+            console.log(result.insertedCount);
+            res.send(result.insertedCount);
+        })
+
+    })
+
+    app.get('/products', (req, res) => {
+        productsCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+    })
+
+
+    app.get('/product/:key', (req, res) => {
+        productsCollection.find({key: req.params.key})
+        .toArray((err, documents) => {
+            res.send(documents[0]);
+        })
+    })
+
+    app.post('/productsByKeys', (req, res) => {
+        const productKeys = req.body;
+        console.log(productKeys)
+        productsCollection.find({key: {$in: productKeys}})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+    })
+
+    app.post('/addOrder', (req, res) => {
+        const order = req.body;
+        ordersCollection.insertOne(order)
+        .then(result => {
+            res.send(result.insertedCount > 0);
+        })
+
+    })
+
+
+    app.get('/', (req, res) => {
+        res.send('Hello');
+    })
+
+});
+
+
+app.listen(port)
